6b9e5772ba2ee6fc2d1ad538c8728fa9f725b219
adormap <35962696+adormap@users.noreply.github.com>
2022-02-08 23:51:49 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-$text    = "Hello Check";
+$text    = "https://www.cpc.ncep.noaa.gov/products/precip/CWlink/MJO/ensplume_full.gif";
 $chat_id = "1398584321";
 $api     = "5286849026:AAH_y_m55CFZMK3wHxR2sMIwox-Ub_MTzow";
 
-$url= "https://api.telegram.org/bot$api/sendMessage?chat_id=$chat_id&text=$text";
+$url= "https://api.telegram.org/bot$api/sendMessage?chat_id=$chat_id&text=$text&pars";
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
@@ -23,11 +23,10 @@ if(isset($result['ok'])){
 	}
 
 } else {
-
+	echo "<pre>";
+	print_r($result);
 	echo "Somethings went wrong!";
 }
 
-echo "<pre>";
-print_r($result);
 
 ?>
