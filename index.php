92e67ab223b72bf9d04cd56a028519c75267850d
adormap <35962696+adormap@users.noreply.github.com>
2022-02-09 01:09:26 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -10,12 +10,13 @@ $api		= "5286849026:AAH_y_m55CFZMK3wHxR2sMIwox-Ub_MTzow";
 
 if($text == '/start') {
 	$msg = "Welcome to XpBot.";
+	$url = "https://api.telegram.org/bot$api/sendMessage?chat_id=$chat_id&text=$msg&parse_mode=html";
+
 } else {
-	$msg = "<a href='https://wx.baf.mil.bd/METBSR/images/omar/RadarSingle/mtr.jpg'><img src='https://wx.baf.mil.bd/METBSR/images/omar/RadarSingle/mtr.jpg'></a>";
+	$img	= "https://wx.baf.mil.bd/METBSR/images/omar/Radar/202202090046.jpg";
+	$url	= "https://api.telegram.org/bot$api/sendPhoto?chat_id=1398584321&photo=$img&parse_mode=html";
 }
 
-
-$url 		= "https://api.telegram.org/bot$api/sendMessage?chat_id=$chat_id&text=$msg&parse_mode=html";
 
 file_get_contents($url);
 
