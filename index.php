32a9b71ce52bd14e49cf12087e1aa9ec4eba4573
adormap <35962696+adormap@users.noreply.github.com>
2022-02-09 00:13:19 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -4,9 +4,16 @@ $input 		= file_get_contents('php://input');
 $data  		= json_decode($input);
 $chat_id 	= $data->message->chat->id;
 $text    	= $data->message->text;
-$text    	= $text." Welcome To XpBot ";
-
 $api		= "5286849026:AAH_y_m55CFZMK3wHxR2sMIwox-Ub_MTzow";
+
+
+
+if(isset(Hi)) {
+	$text = $text. "Hello";
+} else {
+	$text = $text. "Other";
+}
+
 
 $url 		= "https://api.telegram.org/bot$api/sendMessage?chat_id=$chat_id&text=$text";
 
